8577c7a2acb07131dd569f060db7db7c4c2771d3
MarcinTester <marcintester19@gmail.com>
2021-03-22 23:55:28 +0100

    Revert "git test"
    This reverts commit cb45013959bfd6b1e57ba87a55405f3731bb2da3.

--- src/test/java/Tests/HomesTab.java
+++ src/test/java/Tests/HomesTab.java
@@ -19,25 +19,36 @@ public class HomesTab extends Base {
 	{
 		
 		driver = initializeDriver();
-		visit("https://www.wsb.pl/wroclaw/kontakt");
+		visit("https://www.airbnb.com/s/karpacz/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_"
+					+ "dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_"
+					+ "type=calendar&checkin=2021-03-03&checkout=2021-03-11&source=structured_search_input_header&search_type=filter_change");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
 	}
 	@Test
 	public void calendarTest() throws InterruptedException
 	{	
-		Thread.sleep(2000);
-//		String text1 = driver.findElement(By.xpath("//a[@href='tel=//71376290']")).getText();
-		String test1 = driver.findElement(By.cssSelector("a[href='tel=//71376290']")).getText();
-	//	String text1 = driver.findElement(By.xpath("//body/div[2]/div[1]/main[1]/div[1]/div[1]/div[1]/section[2]/div[1]/div[11]/div[1]/div[1]/p[1]/a[1]")).getText();
-	//	String text2 = driver.findElement(By.xpath("//a[@href='mailto:kancelaria2@wsb.wroclaw.pl']")).getText();
-		String text2 = driver.findElement(By.xpath("//a[contains(text(),'kancelaria2@wsb.wroclaw.pl')]")).getText();
+		WebDriverWait w = new WebDriverWait(driver,5);
+		SearchPage searchPage = new SearchPage(driver);
+		searchPage.getCancellationFlexibilityButton().click();
+		searchPage.getCancellationFlexibilitySwitch().click();
+		Assert.assertEquals("true", searchPage.getCancellationFlexibilitySwitch().getAttribute("aria-checked"));
+		w.until(ExpectedConditions.elementToBeClickable(searchPage.getClearButton()));	
+		searchPage.getClearButton().click();
+		Assert.assertEquals("false", searchPage.getCancellationFlexibilitySwitch().getAttribute("aria-checked"));
+		searchPage.getTypeOfPlaceButton().click();
+		Thread.sleep(1000);
+		searchPage.getEntirePlaceCheckBox().click();
 		
-//		System.out.println(text1);
-		System.out.println(text2);
-//		Assert.assertEquals(text1, text2);
-		
-		driver.close();
+		Assert.assertTrue(searchPage.getEntirePlaceCheckBox().isSelected());
+		searchPage.getPrivateRoomCheckBox().click();
+
+		searchPage.getHotelRoomcheckBox().click();
+
+		searchPage.getSharedRoomcheckBox().click();
+		Thread.sleep(1000);
+		//work in progress
+		//to do: checks
 	}
 	@AfterTest
 	public void tearDown() throws IOException
