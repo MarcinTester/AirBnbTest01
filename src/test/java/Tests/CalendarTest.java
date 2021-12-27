a6f7b569167a16a593a939c94894c245503a0536
MarcinTester <79110642+MarcinTester@users.noreply.github.com>
2021-12-27 18:01:34 +0100

    'test'

--- src/test/java/Tests/CalendarTest.java
+++ src/test/java/Tests/CalendarTest.java
@@ -44,7 +44,7 @@ public class CalendarTest extends Base {
 		landingPage.selectCheckOutDay(checkOutdayNumber);
 
 		
-		//test comment
+		
 		Assert.assertTrue(landingPage.get1Day().isDisplayed());
 		Assert.assertTrue(landingPage.get3Days().isDisplayed());
 		Assert.assertTrue(landingPage.get7Days().isDisplayed());
